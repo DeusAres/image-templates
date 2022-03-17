29584c92063553f1a1397b7b454054ad59c8f718
DeusAres <exa.xxiv@gmail.com>
2022-03-17 18:57:42 +0100

    almost

--- main.py
+++ main.py
@@ -4,17 +4,40 @@ import importlib
 from pathlib import Path
 
 listTemplates = os.listdir(Path(__file__).parents[0] / 'templates')
+listTemplates = [str(Path(each).stem) for each in listTemplates if each != '__pycache__']
 
-layout = [
-    [sg.Text('Template'), sg.Listbox(listTemplates, key='template')]
+startLayout = lambda : [
+    [sg.Text('Template'), sg.Combo(listTemplates, enable_events=True, key='template')]
 ]
 
-window = sg.Window("Template generator", layout)
+endLayout = lambda : [
+    [sg.Push(), sg.Button('Generate'), sg.Push()]
+]
+
+
+middleLayout = ''
+
+window = sg.Window("Template generator", startLayout())
 
 while True:
     event, values = window.read()
 
-    if sg.WINDOW_CLOSED:
+    if event == sg.WINDOW_CLOSED:
         break
+    print(event)
+    if event == 'template':
+        template = importlib.import_module(f"templates.{values['template']}")
+        args = template.main.__code__.co_varnames
+        del middleLayout
+        middleLayout = [startLayout()] + [[sg.Text(each.capitalize()), sg.Push(), sg.Multiline("", key=each),] for each in args] + [endLayout()]
+        window.close()
+        window = sg.Window("Template generator", middleLayout)
 
-#template = importlib.import(f'template.{values['template']}')
+    if event == 'Generate':
+        image = template.main(*[values[each] for each in args])
+        image.show()
+        
+
+window.close()
+
+
